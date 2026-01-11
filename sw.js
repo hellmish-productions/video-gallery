32bb46eaaa70e66d57ae32c2aef334582d626f5e
hellmish-productions <tomsterg@gmail.com>
2026-01-12 00:42:36 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,54 @@
+// Service Worker for Hellmish Productions PWA
+const CACHE_NAME = 'hellmish-v1';
+
+// Install event - cache essential files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/video-gallery/',
+        '/video-gallery/index.html',
+        '/video-gallery/img/hellmish-productions-banner.png',
+        '/video-gallery/img/batman-icon.jpg',
+        '/video-gallery/videolist.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event - network first, fall back to cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Clone the response
+        const responseClone = response.clone();
+        
+        // Cache the fetched response
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        
+        return response;
+      })
+      .catch(() => {
+        // Network failed, try cache
+        return caches.match(event.request);
+      })
+  );
+});
